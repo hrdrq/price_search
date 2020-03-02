6a868d4a38ae22dfb34e0c331da99cbd847916dc
hrdrq <son.munehiro+1@gmail.com>
2020-03-03 08:22:12 +0900

    let server call parser

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+
+from parser.amazon import Amazon
+from parser.kakaku import Kakaku
+from parser.mercari import Mercari
+from parser.rakuma import Rakuma
+from parser.ruten import Ruten
+from parser.shopee import Shopee
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/search/<platform>')
+def search(platform):
+    word = request.args.get('word')
+    return globals()[platform]().search(word)
 
 app.run(port=5000)
